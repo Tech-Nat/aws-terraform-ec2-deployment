68d152ef25a9b93a560da3e073efbd9e719d9aa1
Natasha Ndlovu <nat.ndlovu21@gmail.com>
2024-03-14 11:14:24 +0200

    new update

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
